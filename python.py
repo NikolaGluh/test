d402c096590dad48bd29b16bef7126c9c5b3938b
NikolaGluh <gluhovicnikola66@gmail.com>
2019-11-17 22:48:46 +0100

    nik is black

--- python.py
+++ python.py
@@ -3,6 +3,8 @@ from random import choice
 # Tic tac toe
 
 toetable = ["toe", "TOE", "Toe", "TOE!", "toe?", "toe.", "toeeeeeeeeeee", "ToE", "toooooooooE"]
+whattable = ["fat", "big", "small", "thicc", "noob", "pro"]
 table = choice(toetable)
+what = choice(whattable)
 
-print(table)
+print(table + what)
