9f58c72fbf4581570cad82f59054b28a7ea1e305
NikolaGluh <gluhovicnikola66@gmail.com>
2019-11-17 21:38:13 +0100

    yes

---
+++ python.py
@@ -0,0 +1,9 @@
+import random
+from random import choice
+
+# Tic tac toe
+
+toetable = ["toe", "TOE", "Toe", "TOE!", "toe?", "toe.", "toeeeeeeeeeee"]
+table = random.choice(toetable)
+
+print(table)
